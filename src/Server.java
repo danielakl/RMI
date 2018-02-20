ddbd6aff0ac3fd225455103b3980314b7065d906
Daniel Klock <danielkl@stud.ntnu.no>
2018-02-20 10:57:22 +0100

    Add printing of binds. Add unbinding all binds on shutdown.

--- src/Server.java
+++ src/Server.java
@@ -1,21 +1,39 @@
 import remote.Storage;
 import remote.StorageImpl;
 
+import javax.swing.JOptionPane;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+/**
+ * Server exposing all RMI remotes.
+ */
 public final class Server {
     public static void main(String[] args) {
         try {
             Storage storage = new StorageImpl();
 
+            // Bind remote objects.
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind("Storage", storage);
 
-            javax.swing.JOptionPane.showMessageDialog(null, "Trykk OK for å stoppe tjeneren.");
-            registry.unbind("Storage");
+            // Print remote objects provided.
+            String[] binds = registry.list();
+            StringBuilder sb = new StringBuilder("Server starting with following bind" +
+                    ((binds.length > 1) ? "s" : "") + ": ");
+            for (int i = 0; i < binds.length; i++) {
+                sb.append(binds[i]);
+                if (i != binds.length - 1) sb.append(", ");
+            }
+            System.out.println(sb.toString());
+
+            // Unbind remote objects.
+            JOptionPane.showMessageDialog(null, "Press OK to stop the server.");
+            for (String bind : binds) {
+                registry.unbind(bind);
+            }
         } catch (NotBoundException | RemoteException e) {
             e.printStackTrace();
         }
